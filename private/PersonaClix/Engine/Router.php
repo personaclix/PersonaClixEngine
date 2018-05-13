df4278e75b1bcccd85095fb496acf7981ec06bcc
Persona Clix <admin@personaclix.com>
2018-05-13 21:38:00 +0100

    Removed an unused variable

--- private/PersonaClix/Engine/Router.php
+++ private/PersonaClix/Engine/Router.php
@@ -20,8 +20,6 @@ class Router {
 		$method = $_SERVER['REQUEST_METHOD'];
 		$route = $_SERVER['REQUEST_URI'];
 
-		$exception = false;
-
 		if(array_key_exists($method, Router::$routes)) {
 			if(array_key_exists($route, Router::$routes[$method])) {
 				return Router::$routes[$method][$route];
