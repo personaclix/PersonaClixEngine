54509affa7bc1b6995b5dd631d62d06c9f888368
Persona Clix <admin@personaclix.com>
2018-05-13 22:06:29 +0100

    Fixed spelling and improved directory separation

--- private/autoload.php
+++ private/autoload.php
@@ -2,11 +2,11 @@
 
 // AutoLoad classes when needed.
 spl_autoload_register(function ($class_name) {
-	// Convert the black slashes used for namespacing to the directory separator used by the system.
+	// Convert the back slashes used for namespacing to the directory separator used by the system.
 	$class_name = str_replace("\\", DIRECTORY_SEPARATOR, $class_name);
 	// Check if requested class has a file with the same name.
-	if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/../private/' . $class_name . '.php'))
+	if(file_exists($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'private' . DIRECTORY_SEPARATOR . $class_name . '.php'))
 		// Load the file as this would most likely have the requested class.
-		require_once $_SERVER['DOCUMENT_ROOT'] . '/../private/' . $class_name . '.php';
+		require_once $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'private' . DIRECTORY_SEPARATOR . $class_name . '.php';
 });
 
