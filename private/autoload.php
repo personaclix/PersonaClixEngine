149070a8460bd253f3272c3a815001b2177fd2e2
Persona Clix <admin@personaclix.com>
2018-05-13 21:39:52 +0100

    Fixed AutoLoad Issue
    Code worked perfectly fine while testing in a local environment on
    Windows, but when I tested it on a Linux based server, it failed.
    
    This was due to the fact that Linux file systems generally use forward
    slashes (/) instead of backward slashes (\) like Windows file systems
    generally use.
    
    I fixed this issue by converting all backward slashes found in the
    requested class name to the directory separator used by the system.

--- private/autoload.php
+++ private/autoload.php
@@ -1,7 +1,12 @@
 <?php
 
+// AutoLoad classes when needed.
 spl_autoload_register(function ($class_name) {
+	// Convert the black slashes used for namespacing to the directory separator used by the system.
+	$class_name = str_replace("\\", DIRECTORY_SEPARATOR, $class_name);
+	// Check if requested class has a file with the same name.
 	if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/../private/' . $class_name . '.php'))
+		// Load the file as this would most likely have the requested class.
 		require_once $_SERVER['DOCUMENT_ROOT'] . '/../private/' . $class_name . '.php';
 });
 
