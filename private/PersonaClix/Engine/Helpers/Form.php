86c6cb17972101ca771cf3c537afa980cbc4c6f4
personaclix <admin@personaclix.com>
2018-05-25 20:48:15 +0100

    Form Helper Input Fields and Comments
    + Added Support for more Options and Attributes for the Input Field Generator.
    + Added more comments to hopefully make future modifications easier.
    
    * Swapped Input Field Function Arguments for an Associative Array of Options.
    * Cleaned up the code for the Input Field Generator.

--- private/PersonaClix/Engine/Helpers/Form.php
+++ private/PersonaClix/Engine/Helpers/Form.php
@@ -6,6 +6,12 @@ class Form {
 
 	private static $method, $action, $name, $fields = [], $csrf_token = "";
 
+	/**
+	 *	Generates and returns the opening HTML Form tag with specified parameters included.
+	 *	@param string Method (GET or POST)
+	 *	@param string Action (The URL or Path to submit to)
+	 *	@param string Name (A name for the form)
+	 */
 	public static function open(String $method = "", String $action = "", String $name = "") {
 		// Ensure method is in uppercase.
 		$method = strtoupper($method);
@@ -22,35 +28,108 @@ class Form {
 		return '<form' . ($method != "" ? ' method="' . $method . '"' : '') . ($action != "" ? ' action="' . $action . '"' : '') . ($name != "" ? ' name="' . $name . '"' : '') . ">\n";
 	}
 
-	public static function input(String $type, String $name = "", String $value = "") {
-		Form::$fields[] = [
-			'type' => $type,
-			'name' => $name,
-			'value' => $value
-		];
+	/**
+	 *	Generates an Input Field for an HTML Form. Takes an Associative Array of Options.
+	 *	Supported Options are: type, name, id, value, class, style, label
+	 *	As long as the options are specified in a Key => Value manner, they can be in any order.
+	 *	The label option is whether or not a label is required for the field. The name or id option is required for labels to work.
+	 *	@param array Optional Array with Options for the Form Field.
+	 *	@return string Input Form Field.
+	 */
+	public static function input(array $options = []) {
+		// Check for and retrive the possible options.
+		$type = array_key_exists('type', $options) ? $options['type'] : "";
+		$name = array_key_exists('name', $options) ? $options['name'] : "";
+		$id = array_key_exists('id', $options) ? $options['id'] : "";
+		$value = array_key_exists('value', $options) ? $options['value'] : "";
+		$class = array_key_exists('class', $options) ? $options['class'] : "";
+		$style = array_key_exists('style', $options) ? $options['style'] : "";
+		$label = array_key_exists('label', $options) ? $options['label'] : "";
 
-		return '<input type="' . $type . '" name="' . $name . '"' . ($value != "" ? ' name="' . $name . '"' : '') . ">\n";
+		// Prepare a variable to hold the output.
+		$output = "";
+
+		// FIELD LABEL
+		// Check if the field has a label specified along with either an ID or a name.
+		if($label != '' && ($id != '' || $name != '')) {
+			// Output the start of the label tag upto the for attribute.
+			$output .= '<label for="';
+			// Determine if the label is for the field's ID or Name.
+			if($id != '')
+				// Its for the ID, output the ID.
+				$output .= $id;
+			else
+				// Its for the Name, output the Name.
+				$output .= $name;
+			// Output the rest of the opening Label tag (also closing the for attribute).
+			$output .= '">';
+			// Output the label text.
+			$output .= $label;
+			// Output the closing Label tag.
+			$output .= '</label>';
+		}
+
+		// THE FIELD
+		// Output the start of the Input field.
+		$output .= '<input';
+		// Check for and output the Type attribute.
+		if($type != '') $output .= ' type="' . $type . '"';
+		// Check for and output the Name attribute.
+		if($name != '')	$output .= ' name="' . $name . '"';
+		// Check for and output the ID attribute.
+		if($id != '') $output .= ' id="' . $id . '"';
+		// If no ID, check if a label and name have been specified, and output name as ID.
+		else if($label != '' && $name != '') $output .= ' id="' . $name . '"';
+		// Check for and output the class attribute.
+		if($class != '') $output .= ' class="' . $class . '"';
+		// Check for and output the style attribute.
+		if($style != '') $output .= ' style="' . $style . '"';
+		// Check for and output the value attribute.
+		if($value != '') $output .= ' value="' . $value . '"';
+		// Output the end of the input tag.
+		$output .= ">\n";
+
+		//return ($label != '' && ($id != '' || $name != '') ? '<label for="' . ($id != '' ? $id : $name) . '">' . $label . '</label>' : '') . '<input' . ($type != "" ? ' type="' . $type . '"' : '') . ($name != "" ? ' name="' . $name . '"' . ($id != "" ? ' id="' . $id . '"' : ($label != '' ? ' id="' . $name . '"' : '')) : ($id != '' ? ' id="' . $id . '"' : '')) . ($value != "" ? ' name="' . $name . '"' : '') . ">\n";
+
+		// Return everything to output.
+		return $output;
 	}
 
+	/**
+	 *	Generate a hidden field with the CSRF Token. Will also generate a token if one doesn't exist.
+	 *	CSRF Tokens are saved in Cookies that last for 5 minutes.
+	 *	@param bool Whether to return only the token itself.
+	 *	@return string Either just the CSRF Token or an HTML Hidden Form Field with the CSRF Token.
+	 */
 	public static function csrf_token(bool $tokenOnly = false) {
+		// Check if the token cookie has not been set yet
 		if(!isset($_COOKIE['csrf_token'])) {
+			// Generate a token based on a SHA1 Hash of the current date and time
 			$token = sha1(date('YmdHis'));
+			// Set the token cookie with the generated token to expire in 5 minutes.
 			setcookie("csrf_token", $token, time() + (60 * 5));
+		// Token has already been set.
 		} else {
+			// Retrieve the token from the cookie.
 			$token = $_COOKIE['csrf_token'];
 		}
+
+		// Is just the token being requested?
+		if ($tokenOnly)
+			// Return just the token.
+			return $token;
 		
-		Form::$fields[] = [
+		// Otherwise generate the hidden field to hold the token
+
+		// Field options
+		$field_options = [
 			'type' => 'hidden',
 			'name' => 'csrf_token',
 			'value' => $token
 		];
 
-		Form::$csrf_token = $token;
-
-		if ($tokenOnly)
-			return $token;
-		return "<input type=\"hidden\" name=\"csrf_token\" value=\"" . $token . "\">\n";
+		// Generate and return the field.
+		return Form::input( $field_options );
 	}
 
 	public static function close() {
